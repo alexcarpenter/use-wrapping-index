422991b5ea816c2ee4c333542714d461c8d5f97a
Alex Carpenter <im.alexcarpenter@gmail.com>
2022-04-12 17:53:30 -0400

    chore: add tests

---
+++ index.test.js
@@ -0,0 +1,38 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import useWrappingIndex from "./dist";
+
+describe("useWrappingIndex", () => {
+  it("should return the correct default index", () => {
+    const { result } = renderHook(() =>
+      useWrappingIndex({ maxIndex: 5, defaultIndex: 3 })
+    );
+    expect(result.current.activeIndex).toBe(3);
+  });
+
+  it("should set active index", () => {
+    const { result } = renderHook(() =>
+      useWrappingIndex({ maxIndex: 5, defaultIndex: 3 })
+    );
+    expect(result.current.activeIndex).toBe(3);
+    act(() => {
+      result.current.setActiveIndex(4);
+    });
+    expect(result.current.activeIndex).toBe(4);
+  });
+
+  it("should return the correct next and previous indexes", () => {
+    const { result } = renderHook(() => useWrappingIndex({ maxIndex: 5 }));
+    expect(result.current.activeIndex).toBe(0);
+    expect(result.current.nextIndex).toBe(1);
+    expect(result.current.previousIndex).toBe(4);
+  });
+
+  it("should move to the next and previous indexes", () => {
+    const { result } = renderHook(() => useWrappingIndex({ maxIndex: 5 }));
+    expect(result.current.activeIndex).toBe(0);
+    act(() => result.current.moveToNextIndex());
+    expect(result.current.activeIndex).toBe(1);
+    act(() => result.current.moveToPreviousIndex());
+    expect(result.current.activeIndex).toBe(0);
+  });
+});
